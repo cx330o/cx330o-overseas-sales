d22c33f2c7420b9b28fb112f26351f67386bdfa2
pisces0230 <yibo.feb21@gmail.com>
2026-03-26 13:04:09 +0900

    PDF生成バックエンドを追加

---
+++ cx330o-crm/erp-extensions/backend/src/settings/useAppSettings.js
@@ -0,0 +1,8 @@
+const useAppSettings = () => {
+  let settings = {};
+  settings['cx330o_app_email'] = 'noreply@cx330o.com';
+  settings['cx330o_base_url'] = 'https://cloud.cx330o.com';
+  return settings;
+};
+
+module.exports = useAppSettings;
